c7081edca5ebb4c8483d5eda6c715e8d3d234961
Carlos Rodríguez <145693019+44devCarlos@users.noreply.github.com>
2024-07-22 22:02:01 -0500

    Manifesto xD

--- app/src/main/java/com/example/maravillasdelanaturaleza/MainActivity.java
+++ app/src/main/java/com/example/maravillasdelanaturaleza/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.maravillasdelanaturaleza;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
     }
 }
